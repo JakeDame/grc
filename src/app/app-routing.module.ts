0c385d4f489a69a831523ba02a04902024e8a92c
Jake Dame <jakedame1@gmail.com>
2019-04-15 20:28:07 -0700

    cleaned up the routing, still not working

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+    {path: 'contact' , component: ContactComponent},
+    {path: 'home' , component: HomeComponent}
+];
 
 @NgModule({
-  exports: [ RouterModule ],
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { RouterModule, Routes } from '@angular/router';
 import { ContactComponent } from './contact/contact.component';
 import { HomeComponent } from './home/home.component'; 
 
-const appRoutes: Routes = [
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-  { path: 'contact', component: ContactComponent },
-];
-
 @NgModule({
+  declarations: [ AppComponent, ContactComponent, HomeComponent ],
   imports: [ 
-    RouterModule.forRoot(
-    appRoutes, 
-    { enableTracing: true }
-    ),
-    BrowserModule ],
-  declarations: [ AppComponent, HomeComponent ],
+    BrowserModule,
+    AppRoutingModule
+  ],
   providers: [],
   bootstrap: [ AppComponent ]
 })
