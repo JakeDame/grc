b1dec1ca91c5bfad7218b5caeb4993af44ad5f3b
Jake Dame <jakedame1@gmail.com>
2019-05-14 17:40:14 -0700

    Links work!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContactComponent } from './contact/contact.component';
 import { HomeComponent } from './home/home.component'; 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatMenuModule } from '@angular/material';
 
 @NgModule({
   declarations: [ AppComponent, ContactComponent, HomeComponent ],
   imports: [ 
     BrowserModule,
+    BrowserAnimationsModule,
+    MatMenuModule,
     AppRoutingModule
   ],
   providers: [],
