d006ac77066bba447db7379b23e4391288eb9748
Jake Dame <jakedame1@gmail.com>
2019-09-19 14:56:03 -0700

    Updated Navabr

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,11 @@
 import { Component } from '@angular/core';
-import { IImage } from 'ng-simple-slideshow/src/app/modules/slideshow/IImage';
-
 
 @Component({
   selector: 'app-root', 
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'Green Room Construction';
-  imageUrls: (string | IImage)[] = [
-  ];
 }
